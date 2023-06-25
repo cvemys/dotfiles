a704fa7ee4f0fd34dbc2160c5bcb675349d0a627
Amir Zolfaghari <cvemys@gmail.com>
2023-06-25 18:44:19 +0330

    use relative line numbers in neovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,6 +5,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.o.hlsearch = false
 vim.wo.number = true
+vim.wo.relativenumber = true
 vim.o.mouse = "a"
 vim.o.clipboard = "unnamedplus"
 vim.o.breakindent = true
